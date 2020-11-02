67e55b1ed4b2492e5704ef0c4214729dfe71b1fe
Mike Hobi <mikehobizal@gmail.com>
2020-11-02 12:20:04 -0800

    commit

--- maths/primes.js
+++ maths/primes.js
@@ -1,17 +1,15 @@
 const canvasSketch = require('canvas-sketch');
 
 const { lerp } = require('canvas-sketch-util/math');
-const { setSeed, noise2D } = require('canvas-sketch-util/random');
+const { noise2D } = require('canvas-sketch-util/random');
 
 const settings = {
-  animate: true,
-  duration: 3,
   dimensions: [ 2048, 2048 ]
 };
 
 const x = 100
 const y = 100
-const z = 500
+const z = 1000
 
 function test_prime(n)
 {
@@ -38,8 +36,8 @@ function test_prime(n)
 
 
 const sketch = () => {
-  return ({ context, width, height, playhead }) => {
-    context.fillStyle = 'black';
+  return ({ context, width, height }) => {
+    context.fillStyle = 'blue';
     context.fillRect(0, 0, width, height);
 
     // for (let u = 0; u < x; u++) {
@@ -68,14 +66,29 @@ const sketch = () => {
       x: width / 2,
       y: height / 2,
     }
-    setSeed(2)
+    context.strokeStyle = 'white';
     context.translate(center.x, center.y)
     // context.translate(50, 50)
     for (let i = 0; i < z; i++) {
       context.strokeStyle = 'white';
-      context.rotate(noise2D(center.x + i, center.y + i, 0.03 * playhead * 20.0, 10.0) * 360 / 180)
-      context.translate(0, i * 0.5)
+      // context.translate(center.x + (i * 5), center.y + (i * 5))
+      if (test_prime(i)) {
+        //context.rotate(90 * Math.PI / 180 + (noise2D(center.x + i, center.y + i) * 0.35))
+        context.rotate(noise2D(center.x + i, center.y + i, 0.1) * 360 / 180)
+      }
+      context.translate(0, i * 0.05)
       context.lineTo(0, 0)
+      // var inc = 10
+      // var x = center.x + (i * inc) 
+      // var y = center.y + (i * inc)
+      // var mod = i % 2 == 0 ? -1 : 1
+      // if (test_prime(i)) {
+      //   x = center.x + (inc * i)
+      // }
+      // else {
+      //   y = center.y + (inc * i)
+      // }
+      // context.lineTo(x,y)
       context.stroke()
     }
 
